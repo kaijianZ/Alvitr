e5d95c114f4c81e381df7365045fe18b3ecc3c29
kaijianZ <zhongk@rpi.edu>
2017-03-31 13:32:17 -0400

    get pid for the pics

---
+++ getURL.py
@@ -0,0 +1,15 @@
+import requests
+import re
+
+# img_data = requests.get("https://avatars3.githubusercontent.com/u/14187523?v=3&s=460").content
+# with open('image_name.jpg', 'wb') as handler:
+#     handler.write(img_data)
+
+# def getid(r18=False, date=''):
+
+URL = requests.get('http://www.pixiv.net/ranking.php?mode=daily')
+
+content = URL.text
+pattern = re.compile('(?<=data-id=")\d*(?=">)')
+dataids = re.findall(pattern, content)
+print(dataids)
