c14c125370affe7a3f928f9298af6f3432f7854b
kaijianZ <zhongk@rpi.edu>
2017-04-10 00:12:13 -0400

    README updated

--- alvitr.py
+++ alvitr.py
@@ -4,7 +4,10 @@ import saveimg
 import threading
 
 pids = getpids.getpids()
+print('pids retrieved')
+
 cookies = login.login()
+print('cookies retrieved')
 
 for slice in range(10):
     t = threading.Thread(target=saveimg.saveimg, args=(pids[slice * 5: slice * 5 + 5], cookies))
