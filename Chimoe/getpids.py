a65e91e999488428a46f772c2e9028f85c0661b4
Chimoe <hanguangyu12450@gmail.com>
2017-05-05 19:54:40 -0400

    Add files and description document of Zifeng Su

---
+++ Chimoe/getpids.py
@@ -0,0 +1,18 @@
+﻿import requests
+import re
+
+def Choose(self,opener):     #Deside if get ranking or sth
+    if (self.p_choice == 'ranking'):  
+        try:  
+            p_page = opener.open(self.p_ranking_url)   #p_ranking_url = 'hthttp://www.pixiv.net/ranking.php?mode=daily?
+			pattern = re.compile('data-id="(\d*)">')
+
+			return pattern.findall(daily_list_page.text)
+			except:
+				print"没有，滚"
+		else:
+		#following:https://www.pixiv.net/bookmark.php?type=user&id=USERID
+		#USERID is the numbers (user_pid)
+		#TAG:https://www.pixiv.net/search.php?s_mode=s_tag_full&word=TAG
+		#RECOMMENDATION ETC
+			print"现在还没有 滚"
